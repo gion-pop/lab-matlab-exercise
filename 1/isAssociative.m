4f1d40500910c6d6dca26969d6d3702d3e5d9628
Yuya ONUMA <yohnuma@gion-xy.net>
2015-04-10 17:21:30 +0900

    proof 2.32
    Associative law of matrix

---
+++ 1/isAssociative.m
@@ -0,0 +1,16 @@
+function booleanMatrix = isAssociative()
+    A = [
+        1, 2;
+        3, 4;
+    ];
+    B = [
+        2, 3;
+        4, 5;
+    ];
+    C = [
+        3, 4;
+        5, 6;
+    ];
+
+    booleanMatrix = (A * B) * C == A * (B * C);
+end
